f29e70061bd6eafac5790dd83ebbfb6a278dace2
Galymzhan Dosmagambet <galymzhan.dosmagambet@oracle.com>
2020-05-19 19:08:13 +0200

    second commit

--- src/nastya/Controllers/СounteractionController.java
+++ src/nastya/Controllers/СounteractionController.java
@@ -3,16 +3,22 @@ package nastya.Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
 import nastya.Threat;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class СounteractionController  {
@@ -27,15 +33,29 @@ public class СounteractionController  {
     Label label;
 
     @FXML
-    String chosenThreat;
+    Label recommendation;
+
+    ArrayList<CheckBox> reactions = new ArrayList<>();
+
+    public static final List<String> types = Collections.unmodifiableList(
+            new ArrayList<String>() {{
+                add("Административные");
+                add("Организационные");
+                add("Информационно-оперативные");
+                add("Юридические");
+                add("Социально-психологические");
+                add("Охранные");
+                add("IT-технологии безопасности");
+            }});
 
     public void initData(Threat threat) {
         label.setText(threat.getName());
 
         for (int i = 0; i < threat.getReactions().size(); i++) {
             CheckBox option = new CheckBox();
-            option.setId(Integer.toString(i));
-            option.setText(threat.getReactions().get(i));
+            option.setId(threat.getReactions().get(i));
+            option.setText(types.get(i));
+            reactions.add(option);
 
             options.getChildren().add(option);
         }
@@ -66,4 +86,33 @@ public class СounteractionController  {
         mainStage.show();
     }
 
+    public void checkout(ActionEvent event) {
+        Label headerLbl = new Label("ScrollPane");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < reactions.size(); i++) {
+            if (reactions.get(i).isSelected()) {
+                sb.append(types.get(i));
+                sb.append('\n');
+                sb.append(reactions.get(i).getId());
+                sb.append("\n---------------------------------------------\n");
+            }
+        }
+        recommendation.setText(sb.toString());
+
+//        ScrollPane scrollPane = new ScrollPane(textLbl);
+//        scrollPane.setPrefViewportHeight(150);
+//        scrollPane.setPrefViewportWidth(200);
+
+//        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, headerLbl, scrollPane);
+//
+//        layout.getChildren().add(root);
+
+
+        Stage mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        //mainStage.setScene(threatsScene);
+        mainStage.setMaximized(true);
+        mainStage.show();
+    }
 }
